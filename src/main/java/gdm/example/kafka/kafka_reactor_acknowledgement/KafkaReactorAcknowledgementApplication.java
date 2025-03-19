ac043922b1436ee7e1f2005157fbc7190e3af127
Gordon Macrae <macraegordon@hotmail.com>
2025-03-19 16:56:33 +0000

    initial commit

---
+++ src/main/java/gdm/example/kafka/kafka_reactor_acknowledgement/KafkaReactorAcknowledgementApplication.java
@@ -0,0 +1,13 @@
+package gdm.example.kafka.kafka_reactor_acknowledgement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaReactorAcknowledgementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaReactorAcknowledgementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gdm/example/kafka/kafka_reactor_acknowledgement/KafkaReactorAcknowledgementApplicationTests.java
@@ -0,0 +1,13 @@
+package gdm.example.kafka.kafka_reactor_acknowledgement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaReactorAcknowledgementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
